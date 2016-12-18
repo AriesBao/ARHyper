dabc623d06ff0195c7e1d1101ec29eed9e312332
Arise <baorishou@163.com>
2016-12-18 17:45:30 +0800

    Add git ignore

---
+++ ARHyperDemo/ARHyperDemo/ARViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARViewController.h
+//  ARHyperDemo
+//
+//  Created by Arise on 2016/12/18.
+//  Copyright © 2016年 Arise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARViewController : UIViewController
+
+@end
